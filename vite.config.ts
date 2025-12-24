992edbfcc09f6da76aaa7ee7943dd74cb989f103
NguyenKeLuong <nguyenluongd3k@gmail.com>
2025-12-25 01:11:20 +0700

    fix base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/greeting-animation-art/", // 👈 DÒNG QUYẾT ĐỊNH
   server: {
     host: "::",
     port: 8080,
